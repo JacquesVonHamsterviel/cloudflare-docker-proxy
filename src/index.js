ce7a6cb21ee7ddc2105c62b5781c0a53f7b4ece7
Jacques von Hämsterviel <59958130+JacquesVonHamsterviel@users.noreply.github.com>
2024-09-26 21:58:36 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.kkk.plus": dockerHub,
+  "quay.kkk.plus": "https://quay.io",
+  "gcr.kkk.plus": "https://gcr.io",
+  "k8s-gcr.kkk.plus": "https://k8s.gcr.io",
+  "k8s.kkk.plus": "https://registry.k8s.io",
+  "ghcr.kkk.plus": "https://ghcr.io",
+  "cloudsmith.kkk.plus": "https://docker.cloudsmith.io",
+  "ecr.kkk.plus": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
